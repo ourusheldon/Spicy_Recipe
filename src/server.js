84d0ed155a7860d36151cba33d78d259bbe1b7b2
ourusheldon <ourusheldon@gmail.com>
2025-07-01 22:50:09 +0300

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,10 +3,13 @@ import { ENV } from './config/env.js';
 import { db } from './config/db.js';
 import { favorites } from './db/schema.js';
 import { eq, and } from "drizzle-orm";
+import job from './config/cron.js';
 
 
 const app = express();
 const PORT = ENV.PORT || 5001;
+
+if(ENV.NODE_ENV === "production") job.start()
 
 app.use(express.json());
 
